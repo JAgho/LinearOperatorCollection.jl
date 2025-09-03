c5b9e4a44e22d2356bf57f069b9277b7dc0c6e81
JAgho <gholamja@cardiff.ac.uk>
2025-09-03 15:57:48 +0100

    added function constructors to nufft dir

--- ext/LinearOperatorNFFTExt/DirectionalNFFTOp.jl
+++ ext/LinearOperatorNFFTExt/DirectionalNFFTOp.jl
@@ -12,9 +12,9 @@ generates a `DirNFFTOpImpl` which evaluates the MRI Fourier signal encoding oper
 * (`kargs`)                                 - additional keyword arguments
 """
 function LinearOperatorCollection.DirNFFTOp(::Type{T};
-    shape::Tuple, nodes::AbstractMatrix{U}, dims::Union{UnitRange{Int64}, Integer}, toeplitz=false, oversamplingFactor=1.25, 
+    shape::Tuple, nodes::AbstractMatrix{U}, dims::Union{UnitRange{Int64}, Integer}, kshape, toeplitz=false, oversamplingFactor=1.25, 
    kernelSize=3, kargs...) where {U <: Number, T <: Number}
-  return DirNFFTOpImpl(shape, nodes, dims; toeplitz, oversamplingFactor, kernelSize, kargs... )
+  return DirNFFTOpImpl(shape, nodes, dims, kshape; toeplitz, oversamplingFactor, kernelSize, kargs... )
 end
 
 mutable struct DirNFFTOpImpl{T, vecT, P <: AbstractNFFTPlan} <: NFFTOp{T}
@@ -39,16 +39,19 @@ end
 
 LinearOperators.storage_type(op::DirNFFTOpImpl) = typeof(op.Mv5)
 
-function DirNFFTOpImpl(shape::Tuple, tr::AbstractMatrix{T}, dims::Union{UnitRange{Int64}, Integer}; toeplitz=false, oversamplingFactor=1.25, kernelSize=3, S = Vector{Complex{T}}, kargs...) where {T}
+function DirNFFTOpImpl(shape::Tuple, tr::AbstractMatrix{T}, dims::Union{UnitRange{Int64}, Integer}, kshape; toeplitz=false, oversamplingFactor=1.25, kernelSize=3, S = Vector{Complex{T}}, kargs...) where {T}
 
   baseArrayType = Base.typename(S).wrapper # https://github.com/JuliaLang/julia/issues/35543
   plan = plan_nfft(baseArrayType, tr, shape, m=kernelSize, σ=oversamplingFactor, precompute=NFFT.TENSOR,
 		                          fftflags=FFTW.ESTIMATE, blocking=true, dims=dims)
+                          
+  k_produ! = build_produ(kshape)
+  k_ctprodu! = build_ctprodu(kshape)
 
   return DirNFFTOpImpl{eltype(S), S, typeof(plan)}(size(tr,2), prod(shape), false, false
-            , (res,x) -> produ!(res,plan,x)
+            , (res,x) -> k_produ!(res,plan,x)
             , nothing
-            , (res,y) -> ctprodu!(res,plan,y)
+            , (res,y) -> k_ctprodu!(res,plan,y)
             , 0, 0, 0, false, false, false, S(undef, 0), S(undef, 0)
             , plan, toeplitz)
 end
@@ -57,20 +60,32 @@ end
 
 function Base.copy(S::DirNFFTOpImpl{T, vecT, P}) where {T, vecT, P}
   plan = copy(S.plan)
+  k_produ! = S.prod!
+  k_ctprodu! = S.ctprod!
   return DirNFFTOpImpl{T, vecT, P}(size(plan.k,2), prod(plan.N), false, false
-              , (res,x) -> produ!(res,plan,x)
+              , (res,x) -> k_produ!(res,plan,x)
               , nothing
-              , (res,y) -> ctprodu!(res,plan,y)
+              , (res,y) -> k_ctprodu!(res,plan,y)
               , 0, 0, 0, false, false, false, vecT(undef, 0), vecT(undef, 0)
               , plan, S.toeplitz)
 end
 
-function build_produ(kshape, plan)
+function build_produ(kshape)
 
     function produ!(y::AbstractArray, plan::AbstractNFFTPlan, x::AbstractVector) 
         vec(mul!(reshape(y, kshape), plan, reshape(x,kshape)))
       end
+    return produ!
 end
+
+function build_ctprodu(kshape)
+
+  function ctprodu!(x::AbstractVector, plan::AbstractNFFTPlan, y::AbstractArray) 
+      vec(mul!(reshape(x, plan.N), adjoint(plan), reshape(y, kshape)))
+    end
+  return ctprodu!
+end
+
 
 
 function produ!(y::AbstractArray, plan::AbstractNFFTPlan, x::AbstractVector) 
